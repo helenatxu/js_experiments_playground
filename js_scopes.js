716499f7cd0069c01c0f2055936cdacad9d62d48
Helena Moreno Chacon <helenaenred@gmail.com>
2018-12-10 17:42:11 +0100

    Playing with scopes, updating

--- js_scopes.js
+++ js_scopes.js
@@ -14,6 +14,18 @@ const cat = {
 
 };
 
-console.log("cat.ageOneYear()", cat.ageOneYear()); // works
+console.log("cat.ageOneYear()");
+cat.ageOneYear(); // works
 
-console.log("times(10, cat.ageOneYear)", times(10, cat.ageOneYear)); // ReferenceError; this.age is not defined
+console.log("times(10, cat.ageOneYear)");
+times(10, cat.ageOneYear); // ReferenceError; this.age is not defined
+
+
+console.log("times(10, function cat.ageOneYear) -- Function argument");
+// Function argument is different:
+times(10, function () {
+  cat.ageOneYear();
+});
+
+console.log("times(10, bind way)");
+times(10, cat.ageOneYear.bind(cat));
