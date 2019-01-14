4d8491fc9f844e9e82f379b11dddb869844aa6b0
Helena Moreno Chacon <helenaenred@gmail.com>
2019-01-14 20:49:33 +0700

    Studying this example 2 with solutions - bind

--- js_this_ex2.js
+++ js_this_ex2.js
@@ -4,6 +4,7 @@ function times(num, fun) {
   for (let i = 0; i < num; i++) {
     fun(); // call is made "function-style"
   }
+
 }
 
 const dog = {
@@ -15,4 +16,17 @@ const dog = {
 };
 
 console.log("method style call", dog.ageOneYear()); // works
-console.log("function style call", times(10, dog.ageOneYear)); // ReferenceError; this.age is not defined
+// console.log("function style call", times(10, dog.ageOneYear)); // ReferenceError; this.age is not defined
+
+
+// Solutions
+
+// Function argument is different:
+console.log("function style call with method style call inside",
+  times(10, function () {
+  dog.ageOneYear();
+})
+  );
+
+// Bind
+console.log("bind style call", times(10, dog.ageOneYear.bind(dog)));
