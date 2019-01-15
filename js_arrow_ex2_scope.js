bdbb47bf4df380d2dede3937278d098acb091ce9
Helena Moreno Chacon <helenaenred@gmail.com>
2019-01-15 23:58:29 +0700

    Studying arrow functions ex2 with scopes - added solution

--- js_arrow_ex2_scope.js
+++ js_arrow_ex2_scope.js
@@ -1,19 +1,11 @@
 function Dog(name, color) {
     this.name = name;
     this.color = color;
-    this.toys = ['string', 'ball', 'balloon'];
+    this.toys = ['ball', 'rope', 'balloon'];
 };
 
 Dog.prototype.play = function guau() {
-    this.toys.forEach(function(toy) {
-        console.log(`${this.name} plays with ${toy}`);
-    });
+    this.toys.forEach(toy => console.log(`${this.name} plays with ${toy}`));
 };
-
 const Lluna = new Dog('Lluna', 'ginger');
 Lluna.play();
-
-// output
-// undefined plays with string
-// undefined plays with ball
-// undefined plays with balloon
