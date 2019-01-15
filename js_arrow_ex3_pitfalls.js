5c4e31aa819e6b482d811c8401b734637fb6abd8
Helena Moreno Chacon <helenaenred@gmail.com>
2019-01-16 00:58:30 +0700

    Studying arrow functions ex3 - pitfalls

---
+++ js_arrow_ex3_pitfalls.js
@@ -0,0 +1,11 @@
+// Common pitfalls 
+
+// Fat arrows are anonymous, they can't have names
+
+// sayHello(name) => console.log(`Hi, ${name}!`); // SyntaxError
+(name) => console.log(`Hi, ${name}!`); // correct
+const sayHello = (name) => console.log(`Hi, ${name}!`); // you can assign it to a variable
+
+sayHello('Helena');
+
+
