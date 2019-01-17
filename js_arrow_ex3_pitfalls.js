cb7bae60f497a8356798b8d6e78eb88b96794e1f
Helena Moreno Chacon <helenaenred@gmail.com>
2019-01-17 23:42:52 +0700

    Added changes to arrow pitfalls example

--- js_arrow_ex3_pitfalls.js
+++ js_arrow_ex3_pitfalls.js
@@ -4,8 +4,7 @@
 
 // sayHello(name) => console.log(`Hi, ${name}!`); // SyntaxError
 (name) => console.log(`Hi, ${name}!`); // correct
+
 const sayHello = (name) => console.log(`Hi, ${name}!`); // you can assign it to a variable
 
 sayHello('Helena');
-
-
