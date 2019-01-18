cebe9ba6d2b69a31bb8c443f4f5fc439ed79fb06
Helena Moreno Chacon <helenaenred@gmail.com>
2019-01-19 00:40:50 +0700

    Solved js problem sumOfNPrimes

--- problems_js_sumOfNPrimes.js
+++ problems_js_sumOfNPrimes.js
@@ -4,12 +4,39 @@
 // the sum of the first n prime numbers.
 
 function isPrime(number) {
-  var divisor = 3; // as 2 and 3 are primes
+  if (number < 2) return false;
+  var divisor = 2; // as 2 is prime
   while (divisor < number) {
-    divisor ++;
     if (number % divisor === 0)
       return false;
-    else if ((number/divisor) < divisor) break;
+    else if ((number/divisor) < divisor) 
+      break;
+    divisor ++;
   }
   return true;
 }
+
+function firstNPrimes(numberPrimes) {
+  let primesList = [];
+  let countPrimes = 1;
+  for (let number = 2; countPrimes <= numberPrimes; number++) {
+    if (isPrime(number)) {
+      countPrimes ++;
+      primesList.push(number);
+      
+    } 
+  }
+  return primesList;
+}
+
+function sumOfNPrimes(number) {
+  let primesArray = firstNPrimes(number);
+  let sumArray = 0;
+  primesArray.forEach( el => sumArray+=el);
+  return sumArray;
+}
+
+console.log('sumOfNPrimes 0 -->', sumOfNPrimes(0));
+console.log('sumOfNPrimes 1 -->', sumOfNPrimes(1));
+console.log('sumOfNPrimes 4 -->', sumOfNPrimes(4));
+
