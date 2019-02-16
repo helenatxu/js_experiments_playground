d4b9af850ee36fc6ee511e905ad66cf496146a8f
Helena Moreno Chacon <helenaenred@gmail.com>
2019-02-16 14:48:53 +0700

    Studying js objects prototype and constructor

--- js_objects_properties.js
+++ js_objects_properties.js
@@ -1,34 +1,42 @@
 function Dog(name, age, color) {
-  this.name = name;
-  this.age = age;
-  this.color = color;
+	this.name = name;
+	this.age = age;
+	this.color = color;
 }
 
-// We set instance methods for the object on the prototype
-Dog.prototype.bark = function() {
-  console.log(this.name + " says: Woof, woof!!!");
-};
+// We create a new prototype to set properties and instance methods for the object
+Dog.prototype = {
+  // constructor: Dog,  // define the constructor property
+	numLegs: 4,
 
-Dog.prototype.whichColor = function () {
-  console.log("The dog " + this.name + " is: " + this.color);
+  whichColor: function () {
+    console.log("The dog " + this.name + " is: " + this.color);
+  },
+  bark: function() {
+    console.log(this.name + " says: Woof, woof!!!");
+  }
 };
-
-Dog.prototype.numLegs = 4;
 
 let doggie = new Dog("Lluna", 5, "brown");
 
+
+Dog.prototype.numEyes = 2;
 
 // adds all of the own properties to ownProps and all of the prototype properties to prototypeProps
 let ownProps = [];
 let prototypeProps = [];
 
 for (let property in doggie) {
-  if(doggie.hasOwnProperty(property)) {
-    ownProps.push(property);
-  } else {
-    prototypeProps.push(property);
-  }
+	if(doggie.hasOwnProperty(property)) {
+		ownProps.push(property);
+	} else {
+		prototypeProps.push(property);
+	}
 }
 
 console.log("ownProps: ", ownProps); // prints ["name"]
 console.log("prototypeProps: ", prototypeProps); // prints ["numLegs"]
+
+console.log("Test ", doggie.constructor);
+
+doggie.whichColor();
